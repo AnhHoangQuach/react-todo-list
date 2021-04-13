b3e6760759be18ec07f8f12cbb519fb3eedb917a
AnhHoangQuach <hoanganh36.work@gmail.com>
2021-04-14 00:22:27 +0700

    feat: add sort and search complete todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TaskForm from './components/TaskForm';
-import Control from './components/Control';
+import TaskControl from './components/TaskControl';
 import TaskList from './components/TaskList';
 
 class App extends Component{
@@ -15,6 +15,9 @@ class App extends Component{
 				status: -1,
 			},
 			keyword: '',
+			itemEditing: null,
+			sortBy: 'name',
+			sortValue: 1,
 		}
 	}
 
@@ -131,11 +134,20 @@ class App extends Component{
 	}
 
 	onSearch = (keyword) => {
-		console.log(keyword)
+		this.setState({
+			keyword: keyword,
+		})
+	}
+
+	onSort = (sortBy, sortValue) => {
+		this.setState({
+			sortBy: sortBy,
+			sortValue: sortValue,
+		})
 	}
 
 	render() {
-		var { tasks, isDisplayForm, taskEditing, filter } = this.state;
+		var { tasks, isDisplayForm, taskEditing, filter, keyword, sortBy, sortValue } = this.state;
 		if(filter) {
 			if (filter.name) {
 				tasks = tasks.filter((task) => {
@@ -148,6 +160,24 @@ class App extends Component{
 				} else {
 					return task.status === (filter.status === 1 ? true : false)
 				}
+			})
+		}
+		if(keyword) {
+			tasks = tasks.filter((task) => {
+				return task.name.toLowerCase().indexOf(keyword) !== -1;
+			})
+		}
+		if(sortBy === 'name') {
+			tasks.sort((a,b) => {
+				if (a.name > b.name) return sortValue;
+				else if (a.name < b.name) return -sortValue;
+				else return 0;
+			})
+		} else {
+			tasks.sort((a,b) => {
+				if (a.status > b.status) return -sortValue;
+				else if (a.status < b.status) return sortValue;
+				else return 0;
 			})
 		}
 		var elmTaskForm = isDisplayForm ? <TaskForm onSubmit={this.onSubmit} onCloseForm={this.onCloseForm} task={taskEditing} /> : '';
@@ -163,7 +193,7 @@ class App extends Component{
 						<button type="button" className="btn btn-primary" onClick={this.onToggleForm}>
 							<span className="fa fa-plus mr-5"></span> Thêm Công Việc
 						</button>
-						<Control onSearch={this.onSearch}/>
+						<TaskControl sortBy={sortBy} sortValue={sortValue} onSearch={this.onSearch} onSort={this.onSort}/>
 						<TaskList tasks={ tasks } onFilter={this.onFilter} onUpdate={this.onUpdate} onUpdateStatus={this.onUpdateStatus} onDelete={this.onDelete}/>
 					</div>
 				</div>
